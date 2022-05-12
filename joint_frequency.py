ed0656922b14015ce97b0ae8a305a84937e80449
bosun-further <constantgrower@gmail.com>
2022-05-12 19:02:05 +0800

    add reusability to joint_frequency.py

--- joint_frequency.py
+++ joint_frequency.py
@@ -2,18 +2,20 @@ import pandas as pd
 import sidetable
 import csv
 import json
-trimmed_data_path = 'data_sample/trimmed_user_1_data.csv'
-joint_frequency_path = 'data_sample/joint_frequency.csv'
+trimmed_data_path1 = 'data_sample/trimmed/trimmed_user_1_data.csv'
+trimmed_data_path2 = 'data_sample/trimmed/trimmed_user_2_data.csv'
+joint_frequency_path1 = 'data_sample/joint/joint_frequency_1.csv'
+joint_frequency_path2 = 'data_sample/joint/joint_frequency_2.csv'
 # data_y_frequency_path = 'data_sample/y_frequency_user_1_data.csv'
 # json_data_x_frequency_path = 'data_sample/x_frequency_user_1_data.json'
 # json_data_y_frequency_path = 'data_sample/y_frequency_user_1_data.json'
 
 
-def read_from_csv():
+def read_from_csv(trimmed_data_path, joint_frequency_path):
     df = pd.read_csv(trimmed_data_path)
     # print(df['x'].value_counts())
     # print(df['y'].value_counts())
-    write_to_csv(df)
+    write_to_csv(df, joint_frequency_path)
     # write_to_json(df)
 
 #  Write to Json Format
@@ -21,7 +23,7 @@ def read_from_csv():
 # writer.writerow(y_frequence)
 
 
-def write_to_csv(df):
+def write_to_csv(df, joint_frequency_path):
     file_to_write = open(joint_frequency_path, 'w')
     writer = csv.writer(file_to_write)
     print(df.value_counts(['x', 'y']))
@@ -39,7 +41,8 @@ def write_to_csv(df):
 
 
 def main():
-    read_from_csv()
+    read_from_csv(trimmed_data_path1, joint_frequency_path1)
+    read_from_csv(trimmed_data_path2, joint_frequency_path2)
 
 
 if __name__ == "__main__":
