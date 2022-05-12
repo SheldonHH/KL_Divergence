8c3b03bdaf94b78af497d1d371e262d6d59fab45
bosun-further <constantgrower@gmail.com>
2022-05-12 18:18:32 +0800

    add functions

--- independent_frequency.py
+++ independent_frequency.py
@@ -11,9 +11,17 @@ json_data_y_frequency_path = 'data_sample/y_frequency_user_1_data.json'
 
 def read_from_csv():
     df = pd.read_csv(trimmed_data_path)
-    print(df['x'].value_counts())
-    print(df['y'].value_counts())
+    # print(df['x'].value_counts())
+    # print(df['y'].value_counts())
+    write_to_csv(df)
+    write_to_json(df)
 
+#  Write to Json Format
+
+# writer.writerow(y_frequence)
+
+
+def write_to_csv(df):
     file_to_write = open(data_x_frequency_path, 'w')
     writer = csv.writer(file_to_write)
     file_to_write.write(df['x'].value_counts().to_csv(header=False))
@@ -21,12 +29,12 @@ def read_from_csv():
     file_to_write = open(data_y_frequency_path, 'w')
     file_to_write.write(df['y'].value_counts().to_csv(header=False))
 
-#  Write to Json Format
+
+def write_to_json(df):
     json_file_to_write = open(json_data_x_frequency_path, 'w')
     json_file_to_write.write(json.dumps(df['x'].value_counts().to_dict()))
     json_file_to_write = open(json_data_y_frequency_path, 'w')
     json_file_to_write.write(json.dumps(df['y'].value_counts().to_dict()))
-# writer.writerow(y_frequence)
 
 
 def main():
