90c9c2c904ecf759c84272f005626c64e6dbf45e
bosun-further <constantgrower@gmail.com>
2022-05-13 00:15:45 +0800

    add twoGauss

---
+++ lmfit_twoGauss.py
@@ -0,0 +1,60 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.interpolate import griddata
+
+import lmfit
+from lmfit.lineshapes import gaussian2d, lorentzian
+
+
+joint_frequency_path1 = 'data_sample/joint/joint_frequency_1.csv'
+joint_frequency_path2 = 'data_sample/joint/joint_frequency_2.csv'
+
+sg = []
+il = []
+au = []
+file_data1 = np.loadtxt(joint_frequency_path1,
+                        delimiter=',', skiprows=1)
+x_sorted_data = file_data1[file_data1[:, 0].argsort()]
+print(x_sorted_data)
+y_sorted_data = file_data1[file_data1[:, 1].argsort()]
+print(y_sorted_data)
+
+for col1, col2, col3 in file_data1:
+    sg.append(col1)
+    il.append(col2)
+    au.append(col3)
+npa = np.asarray(sg, dtype=np.float32)
+print(npa)
+npb = np.asarray(il, dtype=np.float32)
+npc = np.asarray(au, dtype=np.float32)
+x = npa
+y = npb
+z = npc
+
+
+# npoints = 10000
+# np.random.seed(2021)
+# x = np.random.rand(npoints)*10 - 4
+# y = np.random.rand(npoints)*5 - 3
+# z = gaussian2d(x, y, amplitude=30, centerx=2,
+#                centery=-.5, sigmax=.6, sigmay=.8)
+# z += 2*(np.random.rand(*z.shape)-.5)
+error = np.sqrt(z+1)
+print(x.max())
+X, Y = np.meshgrid(np.linspace(x.min(), x.max(), 1000),
+                   np.linspace(y.min(), y.max(), 1000))
+Z = griddata((x, y), z, (X, Y), method='linear', fill_value=0)
+
+fig, ax = plt.subplots()
+art = ax.pcolor(X, Y, Z, shading='auto')
+plt.colorbar(art, ax=ax, label='z')
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+# plt.show()
+
+model = lmfit.models.Gaussian2dModel()
+params = model.guess(z, x, y)
+print(params)
+result = model.fit(z, x=x, y=y, params=params, weights=1/error)
+lmfit.report_fit(result)
+print(result)
--- twoGauss.py
+++ twoGauss.py
@@ -5,19 +5,20 @@ from numpy import savetxt
 import pickle
 
 # You'll obviously need to reshape the output for plotting, e.g:
-
 joint_frequency_path1 = 'data_sample/joint/joint_frequency_1.csv'
 joint_frequency_path2 = 'data_sample/joint/joint_frequency_2.csv'
 
 
 def twoD_Gaussian(point, amplitude, xo, yo, sigma_x, sigma_y, theta, offset):
+    x = point[0]
+    y = point[1]
     xo = float(xo)
     yo = float(yo)
     a = (np.cos(theta)**2)/(2*sigma_x**2) + (np.sin(theta)**2)/(2*sigma_y**2)
     b = -(np.sin(2*theta))/(4*sigma_x**2) + (np.sin(2*theta))/(4*sigma_y**2)
     c = (np.sin(theta)**2)/(2*sigma_x**2) + (np.cos(theta)**2)/(2*sigma_y**2)
-    g = offset + amplitude*np.exp(- (a*((point[0]-xo)**2) + 2*b*(point[0]-xo)*(point[1]-yo)
-                                     + c*((point[1]-yo)**2)))
+    g = offset + amplitude * \
+        np.exp(- (a*((x-xo)**2) + 2*b*(x-xo)*(y-yo) + c*((y-yo)**2)))
     return g.ravel()
 
 
@@ -33,47 +34,44 @@ def main():
     file_data1 = np.loadtxt(joint_frequency_path1,
                             delimiter=',', skiprows=1)
     sg = []
+    nz = []
+    au = []
     il = []
 
     for col1, col2, col3 in file_data1:
         sg.append([col1, col2])
-        il.append([col3])
-    npa = np.asarray(sg, dtype=np.float32)
-    npb = np.asarray(il, dtype=np.float32)
+        nz.append(col1)
+        au.append(col2)
+        il.append(col3)
+    # npa = np.asarray(sg, dtype=np.float32)
+    # npb = np.asarray(il, dtype=np.float32)
 
     x = np.linspace(0, 200, 201)
     y = np.linspace(0, 200, 201)
     x, y = np.meshgrid(x, y)
-    arr = np.stack((x, y), axis=1)
+    arr = np.stack((nz, au), axis=1)
     print(arr)
     # output = open('test.pkl', 'wb')
     # pickle.dump(arr, output)
     # output.close()
-    data = twoD_Gaussian(arr, 3, 100, 100, 20, 40, 0, 10)
-
-# print(data)
-# plot twoD_Gaussian data generated above
-# plt.figure()
-# c = plt.imshow(npa.reshape(16, 73))
-# # plt.imshow(np.dstack([data.reshape(22, 22)]*3))
-# plt.colorbar(c)
-# plt.show()
-# Do the fitting as before:
-
+    data = twoD_Gaussian(arr, 3, 10, 10, 2, 2, 0, 10)
+    print(data.size)
+    print(data)
 
 # add some noise to the data and try to fit the data generated beforehand
     initial_guess = (3, 100, 100, 20, 40, 0, 10)
-
     data_noisy = data + 0.2*np.random.normal(size=data.shape)
-    # print(data_noisy)
 
-    popt, pcov = curve_fit(twoD_Gaussian, data, data_noisy, p0=initial_guess)
-    # print(data_noisy)
-    # print(type(data_noisy))
-    # And plot the results:
-    # print(popt)
 
+# weighted arithmetic mean (corrected - check the section below)
+    mean = sum(x * y) / sum(y)
+    sigma = np.sqrt(sum(y * (x - mean)**2) / sum(y))
+
+    popt, pcov = curve_fit(twoD_Gaussian, x, y, p0=[
+                           max(y), mean, sigma], maxfev=500000)
+    popt, pcov = curve_fit(twoD_Gaussian, data, il, p0=initial_guess)
     data_fitted = twoD_Gaussian(arr, *popt)
+    print(popt)
 
 
 if __name__ == "__main__":
