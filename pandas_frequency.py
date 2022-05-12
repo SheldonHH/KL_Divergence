9979c5d99cb450066a85d8061a249727989d98b8
bosun-further <constantgrower@gmail.com>
2022-05-12 17:10:05 +0800

    change variable name

--- pandas_frequency.py
+++ pandas_frequency.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import sidetable
-raw_data_path = 'data_sample/trimmed_user_1_data.csv'
+trimmed_data_path = 'data_sample/trimmed_user_1_data.csv'
 
 
 def read_from_csv():
-    df = pd.read_csv(raw_data_path)
+    df = pd.read_csv(trimmed_data_path)
     print(df['x'].value_counts())
 
 
