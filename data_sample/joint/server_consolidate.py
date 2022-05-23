9ded0852452f4ef475186b6ef34bf392cf9b94ee
Sheldonhh <xphuangsg@gmail.com>
2022-05-23 13:30:07 +0800

    create server_consolidate.py

--- data_sample/joint/server_consolidate.py
+++ data_sample/joint/server_consolidate.py
@@ -1,16 +1,23 @@
 import json
-import pandas as pd
 
 
 def main():
-    user_list = ["user1", "user2"]
-    df = pd.read_json('user1_params.json')
-    print(df)
-    # f = open('user1_params.json')
-    # data = json.load(f)
-    # f = open('user2_params.json')
-    # data = json.load(f)
-    # print(data)
+    consolidated_params = {}
+    consolidated_entropies = {}
+    users_list = ["user1", "user2"]
+    for user in users_list:
+        params_file_user = user+'_params.json'
+        with open(params_file_user) as json_file:
+            data_dict = json.load(json_file)
+        consolidated_params[user] = data_dict[user]
+
+        entropies_file_user = user+'_entropies.json'
+        with open(entropies_file_user) as json_file:
+            data_dict = json.load(json_file)
+        consolidated_entropies[user] = data_dict[user]
+
+    print(consolidated_params)
+    print(consolidated_entropies)
 
 
 if __name__ == "__main__":
