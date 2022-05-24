b376aa5b96d7e4e667885139c27aebe21046522f
fedproject <xphuang@smu.edu.sg>
2022-05-24 17:41:22 +0800

    add orient="index"

--- data_sample/joint/server_consolidate.py
+++ data_sample/joint/server_consolidate.py
@@ -1,9 +1,9 @@
 import json
 import pandas as pd
 
-w_consolidated_gauss_params_json = 'consolidated_gauss_params.json'
-w_consolidated_entropies_json = 'consolidated_entropies.json'
-w_consolidated_percent_json = 'consolidated_percent.json'
+w_consolidated_gauss_params_json = './consolidated/consolidated_gauss_params.json'
+# w_consolidated_entropies_json = 'consolidated_entropies.json'
+# w_consolidated_percent_json = 'consolidated_percent.json'
 
 
 def write_dict_to_json(dict, json_to_write):
@@ -25,33 +25,33 @@ def main():
             data_dict = json.load(json_file)
         consolidated_params[user] = data_dict[user]
 
-        entropies_file_user = user+'_entropies.json'
-        with open(entropies_file_user) as json_file:
-            data_dict = json.load(json_file)
-        consolidated_entropies[user] = data_dict[user]
+    #     entropies_file_user = user+'_entropies.json'
+    #     with open(entropies_file_user) as json_file:
+    #         data_dict = json.load(json_file)
+    #     consolidated_entropies[user] = data_dict[user]
 
-        entropies_sum_file_user = user+'_entropies_sum.json'
-        with open(entropies_sum_file_user) as json_file:
-            data_dict = json.load(json_file)
-            print(data_dict[user]["0"])
-        consolidated_sum_entropies += float(data_dict[user]["0"])
+    #     entropies_sum_file_user = user+'_entropies_sum.json'
+    #     with open(entropies_sum_file_user) as json_file:
+    #         data_dict = json.load(json_file)
+    #         print(data_dict[user]["0"])
+    #     consolidated_sum_entropies += float(data_dict[user]["0"])
 
-    print(consolidated_params)
-    print(consolidated_entropies)
+    # print(consolidated_params)
+    # print(consolidated_entropies)
     write_dict_to_json(consolidated_params, w_consolidated_gauss_params_json)
-    write_dict_to_json(consolidated_entropies, w_consolidated_entropies_json)
+    # write_dict_to_json(consolidated_entropies, w_consolidated_entropies_json)
 
     # weight of each user
-    consolidated_percent_dict = {}
-    for user in users_list:
-        entropies_sum_file_user = user+'_entropies_sum.json'
-        with open(entropies_sum_file_user) as json_file:
-            data_dict = json.load(json_file)
-            print(data_dict[user]["0"])
-        consolidated_percent_dict[user] = (
-            data_dict[user]["0"])/consolidated_sum_entropies
-    write_profit_weight_to_json(consolidated_percent_dict,
-                                w_consolidated_percent_json)
+    # consolidated_percent_dict = {}
+    # for user in users_list:
+    #     entropies_sum_file_user = user+'_entropies_sum.json'
+    #     with open(entropies_sum_file_user) as json_file:
+    #         data_dict = json.load(json_file)
+    #         print(data_dict[user]["0"])
+    #     consolidated_percent_dict[user] = (
+    #         data_dict[user]["0"])/consolidated_sum_entropies
+    # write_profit_weight_to_json(consolidated_percent_dict,
+    #                             w_consolidated_percent_json)
 
 
 def write_profit_weight_to_json(list, json_to_write):
