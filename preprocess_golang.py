25bc4a5413abea77eaec816f91b379b26331e1e0
Sheldonhh <xphuangsg@gmail.com>
2022-05-26 16:19:57 +0800

    clean up

--- preprocess_golang.py
+++ preprocess_golang.py
@@ -7,7 +7,7 @@ raw_data_path2 = 'data_sample/user_2_data.csv'
 # w_trimmed_data_path2 = 'data_sample/trimmed/trimmed_user_2_data.csv'
 # w_dense_trimmed_data_path1 = 'data_sample/trimmed/dense_trimmed_user_1_data.csv'
 w_dense_trimmed_data_path2 = 'data_sample/trimmed/dense_trimmed_user_2_data.csv'
-w_first_dict_json_path = 'data_sample/dict/first_dict_data.json'
+# w_first_dict_json_path = 'data_sample/dict/first_dict_data.json'
 first_layer_dict = {}
 
 
@@ -78,7 +78,7 @@ def read_from_csv(raw_data_path, trimmed_data_path):
     first_layer_dict.clear()
     first_layer_dict.update(filtered)
     # print(first_layer_dict["2-3"])
-    write_first_dict()
+    # write_first_dict()
 
 
 def main():
@@ -89,12 +89,12 @@ def main():
     middle_index = args[0].rindex('/')
     last_index = len(args[0])-1
     first_half = args[0][0:middle_index+1]
-    print("first_half",first_half)
+    print("first_half", first_half)
     second_half = args[0][middle_index+1: len(args[0])]
     print(second_half)
     print(args[0].rindex('/'))
     w_dense_trimmed_data_path1 = first_half+"trimmed/dense_trimmed_"+second_half
-    print("w_dense_trimmed_data_path1",w_dense_trimmed_data_path1)
+    print("w_dense_trimmed_data_path1", w_dense_trimmed_data_path1)
     read_from_csv(raw_data_path1, w_dense_trimmed_data_path1)
     # read_from_csv(raw_data_path2, w_dense_trimmed_data_path1)
 
