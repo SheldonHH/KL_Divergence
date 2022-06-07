c36b3cf47537f6ded7a2af8281c2652282cebd08
fedproject <xphuang@smu.edu.sg>
2022-06-07 20:21:28 +0800

    add sample_gauss.json

--- user_gauss_params/py/create_slides.py
+++ user_gauss_params/py/create_slides.py
@@ -22,8 +22,19 @@ import csv
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 from numpy import genfromtxt
+import plt_freq_to_gaussian
 from random import sample
+from functools import reduce
 
+def write_dict_to_json(dict, json_to_write):
+    df_params = pd.DataFrame.from_dict(dict)
+    result = df_params.to_json(orient="columns")
+    parsed = json.loads(result)
+    with open(json_to_write, 'w') as convert_file:
+        convert_file.write(json.dumps(parsed))
+
+def findNth(a, b, n):
+    return reduce(lambda x, y: -1 if y > x + 1 else a.find(b, x + 1), range(n), -1)
 
 def sample_subportion(subportion, percent_in_decimal):
   sample_size = int(len(subportion) * percent_in_decimal )
@@ -33,22 +44,50 @@ def sample_subportion(subportion, percent_in_decimal):
   else:
     sample_list = sample(subportion,1)
     print("sample_size", sample_size)
-    return sample_list
-  
+  return sample_list
+
+def zerolistmaker(n):
+    listofzeros = [0] * n
+    return listofzeros
+
+def simulated_height_normal_dist(x, mean, sd):
+    return norm.cdf(x, mean, sd)
+
 def main():
     num_of_slides = 10
     uneven_dir = "/root/KL_Divergence/user_gauss_params/data/uneven/features/"
     uniform_dir = "/root/KL_Divergence/user_gauss_params/data/uniform/features/"
-    dir_str = uniform
-    directory = os.path.join(dir_str)
-    os.chdir(dir_str)   
+    dir_str = uniform_dir
+
     num_with_params = {}
     all_files_dimension_with_params = {}
-    for root,dirs,files in os.walk(directory):
+
+    all_population_gauss = {}
+    freq_directory = os.path.join(dir_str+"freq")
+    os.chdir(dir_str+"freq")   
+    for root,dirs,files in os.walk(freq_directory):
+      for file in files:
+        if file.endswith(".json"):
+              f=open(file, 'r')
+              population_gauss = json.load(f)
+              print('len=-----',len(population_gauss))
+              for uname, user in population_gauss.items():
+                  print(len(user["gauss"][0])/3)
+                  print("uname",uname)
+                  all_population_gauss[uname]=user["gauss"][0]
+                  
+    rounded_data_directory = os.path.join(dir_str)
+    os.chdir(dir_str)   
+    ## create subportion for each file
+    ## sample subportion and generation gmm
+    file_gauss_dict = {}
+    for root,dirs,files in os.walk(rounded_data_directory):
         for file in files:
             dimension_min_with_params = {}
             if file.endswith(".csv") and file.startswith("rounded"):
                 f=open(file, 'r')
+                print(file[findNth(file,"_",1)+1:findNth(file,"_",3)])
+                 
                 data_iter = csv.reader(f)
                 data = [data for data in data_iter]
                 unsorted_list = []
@@ -64,6 +103,7 @@ def main():
                 llen = len(sorted_list)
                 step = (lmx-lmin)/num_of_slides 
                 pos = lmin
+                theuser_sample = []
                 while(pos+step<lmx):
                     lower_bound = pos
                     upper_bound = pos + step
@@ -73,15 +113,45 @@ def main():
                         sub_portion.append(item)
                       if item >= lower_bound and item <= lmx and pos == lmx-step-step:
                         upper_bound = lmx
-                        print("upper_bound",upper_bound)
+                        # print("upper_bound",upper_bound)
                         sub_portion.append(item)
                     percent_in_decimal = 0.1
                     # print(file,lower_bound,"-",upper_bound,": ","before sample size",len(sub_portion),'min',min,'-max',lmx)
-                    # print(sample_subportion(sub_portion, percent_in_decimal))
+                    theuser_sample.extend(sample_subportion(sub_portion, percent_in_decimal))
                     pos += step
-                
-                    
-                 
+                # Xsample = pd.Series(theuser_sample, index='x1')
+                gaussNum = len(all_population_gauss[file[findNth(file,"_",1)+1:findNth(file,"_",3)]])
+                print("value_counts(np.array(theuser_sample))",type(pd.Series(np.array(theuser_sample)).value_counts().to_numpy()))
+                # ready_for_gmm = np.array(theuser_sample)
+
+                # ready_for_gmm = np.random.rand(len(theuser_sample),len(theuser_sample))
+
+                # ready_for_gmm = np.zeros((2,len(theuser_sample)))
+                # ready_for_gmm[0:,] = np.array(theuser_sample)
+                # print("France",pd.Series(np.array(theuser_sample)).value_counts(normalize=True, sort=True))
+                sgg = list(pd.Series(np.array(theuser_sample)).value_counts(normalize=True, sort=True))
+                # print("sggggg",sgg)
+
+
+                ready_for_gmm = np.c_[np.unique(np.array(theuser_sample), return_counts=1)]
+                # print("ready_for_gmm: ",ready_for_gmm)
+                # np.append(np.array(theuser_sample),pd.Series(np.array(theuser_sample)).value_counts().to_numpy())
+                # print("pd.Series(np.array(theuser_sample)).value_counts().to_numpy()",pd.Series(np.array(theuser_sample)).value_counts().to_numpy())
+                gmm = mixture.GaussianMixture(n_components=gaussNum, covariance_type="diag", max_iter=500000).fit(ready_for_gmm)
+                gauss_weights_dict = {}
+                sample_params = []
+                sample_params = zerolistmaker(gaussNum*3)
+                simulated_y_sum = 0
+                for sub_index in range(gaussNum):
+                    sample_params[sub_index*3] = gmm.means_[sub_index][0]
+                    sample_params[sub_index*3+1] = gmm.covariances_[sub_index][0]
+                    sample_params[sub_index*3+2] = simulated_height_normal_dist(gmm.means_[sub_index][0], gmm.means_[sub_index][0], math.sqrt(gmm.covariances_[sub_index][0]))*gmm.weights_[sub_index]
+                # print("sample_params", sample_params)  
+                gauss_weights_dict["gauss"] = sample_params
+                gauss_weights_dict["weights"] = gmm.weights_.tolist()
+                file_gauss_dict[file[findNth(file,"_",1)+1:findNth(file,"_",3)]] = gauss_weights_dict
+        print("file_gauss_dict",file_gauss_dict)
+        write_dict_to_json(file_gauss_dict, "sample_gauss.json")
                 
                 
                 
