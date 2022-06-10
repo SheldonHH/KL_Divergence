a8b842dd98bebca9cd831ac8196b7fb574e77e55
fedproject <xphuang@smu.edu.sg>
2022-06-10 11:31:53 +0800

    write image to correct folder

--- user_gauss_params/main.go
+++ user_gauss_params/main.go
@@ -2,15 +2,15 @@ package main
 
 import (
 	"fmt"
-	ugg "user_gauss_params/userGenerateGauss"
 	"log"
 	"time"
+	ugg "user_gauss_params/userGenerateGauss"
 )
 
 func main() {
 	start := time.Now()
-	raw_data_path := "user_gauss_params/data/user_1_mnist_train.csv"
-	individual_Gauss := ugg.MixGauss(raw_data_path)
+	raw_data_path := "user_gauss_params/data/user_1.csv"
+	individual_Gauss := ugg.GenerateGauss(raw_data_path)
 	fmt.Println("individual_Gauss", individual_Gauss)
 	elapsed := time.Since(start)
 	log.Printf("user_1_mnist_train Gauss generation took %s", elapsed)
