06ee15360b73184b57cf1ea2b23d756ee13fa88f
fedproject <xphuang@smu.edu.sg>
2022-06-10 14:46:57 +0800

    delete final Gaussian file

--- user_gauss_params/main.go
+++ user_gauss_params/main.go
@@ -1,20 +1,42 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"os"
+	"path/filepath"
+	"runtime"
 	"time"
 	ugg "user_gauss_params/userGenerateGauss"
 )
 
+var (
+	_, b, _, _ = runtime.Caller(0)
+	basepath   = filepath.Dir(b)
+	username   = "user_1"
+)
+
 func main() {
 	start := time.Now()
-	raw_data_path := "/root/KL_Divergence/user_gauss_params/data/user_1.csv"
+	fmt.Println(basepath)
+	raw_data_path := basepath + "/data/" + username + ".csv"
 	individual_Gauss := ugg.GenerateGauss(raw_data_path)
 	fmt.Println("individual_Gauss", individual_Gauss)
+	plan, _ := ioutil.ReadFile(basepath + "/data/features/freq/dynamic_features_gauss.json")
+	var data interface{}
+	err := json.Unmarshal(plan, &data)
+	if err != nil {
+		fmt.Print("err:", err)
+	}
+	fmt.Println("data", data)
+	e := os.Remove(basepath + "/data/features/freq/dynamic_features_gauss.json")
+	if e != nil {
+		log.Fatal(e)
+	}
 	elapsed := time.Since(start)
-	log.Printf("user_1_mnist_train Gauss generation took %s", elapsed)
-
+	log.Printf(username+"'s Gauss generation took %s", elapsed)
 	// singapore_Gauss := ugg.GenerateGauss(raw_data_path)
 	// fmt.Println("singapore Gauss", singapore_Gauss)
 }
