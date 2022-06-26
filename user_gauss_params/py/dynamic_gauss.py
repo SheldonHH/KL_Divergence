ac6edbc5bcb5bd0a055e5039a84a670228fe0aa3
Sheldonhh <xphuangsg@gmail.com>
2022-06-27 00:50:57 +0800

    pass in from numpy array instead of path

--- user_gauss_params/py/dynamic_gauss.py
+++ user_gauss_params/py/dynamic_gauss.py
@@ -1,16 +1,26 @@
 from tally import csv_to_jpeg
 from py_torch.FeatureExtractor import extract_features
-from tally import round_data_golang 
+from tally import round_data_golang
 from tally import countFreq_golang
 from tally import freq_to_gauss_golang
 # from tally import features_extractor
+
+
 def main():
-  raw_csv_argv = ["/root/KL_Divergence/user_gauss_params/data/user_4.csv"]
-  raw_data_len = csv_to_jpeg.csv_to_jpeg(raw_csv_argv)
-  extract_features(raw_csv_argv)
-  round_data_golang.round_data(raw_csv_argv)
-  countFreq_golang.count_freq(raw_csv_argv)
-  freq_to_gauss_golang.freq_to_gauss(raw_csv_argv, raw_data_len)
+    numpy_array = np.loadtxt(
+        "/root/KL_Divergence/user_gauss_params/data/user_4.csv", delimiter=",")
+    user_name = "user_4.csv"
+
+
+def dynamic_gauss(numpy_array, user_name):
+    real_raw_csv_argv = [
+        "/root/KL_Divergence/user_gauss_params/data/"]
+    raw_csv_argv = [real_raw_csv_argv[0]+user_name]
+    raw_data_len = csv_to_jpeg.csv_to_jpeg(numpy_array, raw_csv_argv)
+    extract_features(raw_csv_argv)
+    round_data_golang.round_data(raw_csv_argv)
+    countFreq_golang.count_freq(raw_csv_argv)
+    freq_to_gauss_golang.freq_to_gauss(raw_csv_argv, raw_data_len)
 
 
 if __name__ == "__main__":
