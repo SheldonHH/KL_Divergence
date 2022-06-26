9e38c44437974e93feecd7a4ecc920f9b1858eb5
Sheldonhh <xphuangsg@gmail.com>
2022-06-27 00:05:09 +0800

    enable selected user_list in entropy calculation

--- server_entropy_percent/py/tally/server_consolidate_golang.py
+++ server_entropy_percent/py/tally/server_consolidate_golang.py
@@ -2,6 +2,7 @@ import json
 import os
 import sys
 import pandas as pd
+from functools import reduce
 
 
 def write_dict_to_json(dict, json_to_write):
@@ -23,7 +24,11 @@ def user_list_from_dir(dir_in_str):
             continue
     return usergauss_list
 
-def server_consolidate(individual_gauss):
+def findNth(a, b, n):
+    return reduce(lambda x, y: -1 if y > x + 1 else a.find(b, x + 1), range(n), -1)
+
+def server_consolidate(individual_gauss, required_userlist):
+    print("required_userlist", required_userlist)
     args = individual_gauss
     gauss_users_dir = args[0]
     consolidated_params = {}
@@ -31,11 +36,13 @@ def server_consolidate(individual_gauss):
     users_list = user_list_from_dir(gauss_users_dir)
     print("users_list",users_list)
     for user_gauss_file in users_list:
-        # username = user_gauss_file[0: user_gauss_file.rindex('.')]
-        with open(gauss_users_dir+"/"+user_gauss_file) as json_file:
-            data_dict = json.load(json_file)
-            pk = list(data_dict.keys())[0]
-            consolidated_params[pk] = data_dict[pk]
+        if user_gauss_file[0:findNth(user_gauss_file, "_",2)] in required_userlist:
+            # if 'time' in required_userlist :
+            # username = user_gauss_file[0: user_gauss_file.rindex('.')]
+            with open(gauss_users_dir+"/"+user_gauss_file) as json_file:
+                data_dict = json.load(json_file)
+                pk = list(data_dict.keys())[0]
+                consolidated_params[pk] = data_dict[pk]
     w_consolidated_gauss_params_json = gauss_users_dir + "/consolidated/consolidated_gauss_params.json"
     write_dict_to_json(consolidated_params, w_consolidated_gauss_params_json)
 
