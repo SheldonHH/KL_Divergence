e1f20dbc21dcb6e46f4edbfc1fd74eeffd6134a2
Sheldonhh <xphuangsg@gmail.com>
2022-06-27 00:05:09 +0800

    enable selected user_list in entropy calculation

--- server_entropy_percent/py/dynamic_entropy.py
+++ server_entropy_percent/py/dynamic_entropy.py
@@ -3,7 +3,8 @@ from tally import calculate_entropy_golang
 
 def main():
   individual_gauss = ["/root/KL_Divergence/server_entropy_percent/data/users_individual_gauss"] # location where stores individual gauss
-  server_consolidate_golang.server_consolidate(individual_gauss)
+  users_list = ["user_1", "user_2", "user_4"]
+  server_consolidate_golang.server_consolidate(individual_gauss,users_list)
   calculate_entropy_golang.calculate_entropy(individual_gauss)
 
 
