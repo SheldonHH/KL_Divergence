26d96c41cf977c24cfd64ee71007a04ba436e39f
Sheldonhh <xphuangsg@gmail.com>
2022-07-10 15:26:54 +0800

    update

--- user_gauss_params/py/combine_csv.py
+++ user_gauss_params/py/combine_csv.py
@@ -1,9 +1,12 @@
 from this import d
 import pandas as pd
+import numpy as np
 import glob
 import os
 import csv
 import re
+import natsort
+from tally import countFreq
 # from itertools import izip
 
 # setting the path for joining multiple files
@@ -37,7 +40,7 @@ def transpose_csv(Dir):
     counter = 0
     for inputfile in input_file_list:
         user_filename = inputfile[inputfile.rindex('/')+1: inputfile.rindex('.')]
-        outputfile = Dir+'transposed_features/'+ user_filename  +"_transposed.csv"
+        outputfile = Dir+'transposed_features/transposed_'+ user_filename  +".csv"
         a = zip(*csv.reader(open(inputfile, "rt")))
         csv.writer(open(outputfile, "wt")).writerows(a)
                 # open(Dir+'combine/transposed_combined_features.csv', "wt")).writerows(a)
@@ -54,7 +57,7 @@ def transpose_combine(Dir):
     #     open(Dir+'combine/transposed_combined_features.csv', "wt")).writerows(a)
     pd.read_csv(inputfile, header=None).T.to_csv(
         outputfile, header=False, index=False)
-
+    # return 
 
 # def calculate_gauss():
 
@@ -66,25 +69,69 @@ def transpose_combine(Dir):
 
 def main():
     Dir = "/root/KL_Divergence/user_gauss_params/data/"
+    psedo_user_dir = "/root/KL_Divergence/user_gauss_params/data/user_4.csv"
     # transpose individual csv
-    transpose_csv(Dir)
+    # transpose_csv(Dir)
     
-    # csv_file_list = glob.glob(Dir + 'transposed_features/*.csv')
-    # # print(csv_file_list)
-    # # csv_file_list.sort()
-    # # print(sorted(csv_file_list))
-    # csv_file_list.sort(key=lambda f: int(re.sub('\D', '', f)))
+    # #
+    transposed_eachuser = glob.glob(Dir + 'transposed_features/*.csv')
+    # print(transposed_eachuser)
     # print(csv_file_list)
+    # csv_file_list.sort()
+    # print(sorted(csv_file_list))
+    # transposed_eachuser.sort(key=lambda f: int(re.sub('\D', '', f)))
+    # transposed_eachuser.sort(key=lambda f: int(re.sub('\D', '', f)))
+    # transposed_eachuser.sort(key=lambda f: int(''.join(filter(str.isdigit, f))))
+    # transposed_eachuser = natsort.natsorted(transposed_eachuser)
+    # print(transposed_eachuser)
+    # befores = []
     # with open(Dir+'combine/combined_features.csv', 'w') as wf:
-    #     for file in csv_file_list:
+    #     for file in transposed_eachuser:
     #         with open(file) as rf:
     #             for line in rf:
     #                 if line.strip():  # if line is not empty
     #                     if not line.endswith("\n"):
     #                         line += "\n"
-    #                     wf.write(line)
+    #                     wf.write(line) # type string
+    #                     li = list(line.split(" ")) 
+    #                     befores.append(li)
+    #                     print(type(line))
+
+
+
+ 
+    # for tfile in transposed_eachuser:
+    #     user_filename = tfile[tfile.rindex('/')+1: tfile.rindex('.')]
+    #     with open(file) as rf:
+
+
+    # Write one feature file, total feature: 4096
+    # read all user file
+    for feat_counter in range(4096):
+        with open(Dir+"nofeatures/"+str(feat_counter)+"_feature.csv","w") as wf:
+            for file in transposed_eachuser:
+                with open(file) as rf:
+                    for line in rf:
+                        li = list(line.split(","))
+                        wf.write(li[feat_counter]+"\n")
+
+    # afters = zip(*befores)    
+    # # for row in afters:
+    # print(afters)
+    # csv.writer(open(Dir+'combine/transposed_combined_features.csv', "wt")).writerows(afters)       
+    # print(befores)
+    # countFreq([psedo_user_dir], np.array(befores).T[0], 0)
+    # for idx,row in enumerate(np.array(befores).T):
+    #     count_freq([psedo_user_dir], row,idx)
+    #     if idx == 0:
+    #         break
 
     # transpose_combine(Dir)
+    # inputfile = Dir + 'combine/combined_features.csv'
+    # df = pd.read_csv(inputfile, header=None).T
+    # for index, row in df.iterrows():
+    #     print("row", row)
+    #     count_freq(raw_csv_argv, row)
 
 
 if __name__ == "__main__":
