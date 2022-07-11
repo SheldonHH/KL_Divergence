b0ed89007a73d22745d3dec7bc1f3a188092b0fb
Sheldonhh <xphuangsg@gmail.com>
2022-07-11 16:20:08 +0800

    fast combine and igonre large file

--- user_gauss_params/py/D_perform_combine_3.py
+++ user_gauss_params/py/D_perform_combine_3.py
@@ -66,22 +66,53 @@ import time
 #         outputfile, header=False, index=False)
     # return 
 
-def perform_combine(transposed_inputs_dir, target_dir):
+def newline_combine(transposed_inputs_dir, target_fullPath, username):
     extension = 'csv'
     all_filenames = [i for i in glob.glob(transposed_inputs_dir+'*.{}'.format(extension))]
     print(len(all_filenames))
-   #combine all files in the list
-    combined_csv = pd.concat([pd.read_csv(f)+"\n" for f in all_filenames ])
-   #export to csv
-    combined_csv.to_csv( target_dir, index=False, encoding='utf-8-sig')
+    csv_list = []
+    counterrr = 0
+    sorted_filenames = natsort.natsorted(all_filenames)
+    with open(target_fullPath, "w") as f:
+        for file in sorted_filenames:
+            print(counterrr)
+            counterrr+=1
+            with open(file) as fff:
+                f.write(fff.read())
+        # csv_merged = pd.concat(csv_list, ignore_index=True)
 
-# def perform_split_by_col(transposed_inputs_dir, target_dir):
+# 6. Single DF is saved to the path in CSV format, without index column
+    # csv_merged.to_csv(target_fullPath, index=False)
+
+# def perform_combine(transposed_inputs_dir, target_fullPath, username):
+#     extension = 'csv'
+#     all_filenames = [i for i in glob.glob(transposed_inputs_dir+'*.{}'.format(extension))]
+#     print(len(all_filenames))
+#     csv_list = []
+#     counterrr = 0
+#     sorted_filenames = natsort.natsorted(all_filenames)
+#     for file in sorted_filenames:
+#         print(counterrr)
+#         csv_list.append(pd.read_csv(file).assign(File_Name = os.path.basename(file)))
+#         csv_list.append("\n")
+#         counterrr+=1
+#     csv_merged = pd.concat(csv_list, ignore_index=True)
+
+# # 6. Single DF is saved to the path in CSV format, without index column
+#     csv_merged.to_csv(target_fullPath, index=False)
+
+   #combine all files in the list
+#     combined_csv = pd.concat([pd.read_csv(f)+"\n" for f in all_filenames ])
+#    #export to csv
+#     combined_csv.to_csv( target_fullPath, index=False, encoding='utf-8-sig')
+
+# def perform_split_by_col(transposed_inputs_dir, target_fullPath):
 #     extension = 'csv'
 #     all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
 #    #combine all files in the list
 #     combined_csv = pd.concat([pd.read_csv(f)+"\n" for f in all_filenames ])
 #    #export to csv
-#     combined_csv.to_csv( target_dir, index=False, encoding='utf-8-sig')
+#     combined_csv.to_csv( target_fullPath, index=False, encoding='utf-8-sig')
 
 
 def main():
@@ -89,9 +120,9 @@ def main():
     Dir = "/root/KL_Divergence/user_gauss_params/data/"
     username = "user_1"
     transposed_inputs_dir = "/root/KL_Divergence/user_gauss_params/data/transposed_features/"+username+"/"
-    target_dir = "/root/KL_Divergence/user_gauss_params/data/combine/"+username+"_comnbined.csv"
-    perform_combine(transposed_inputs_dir,target_dir)
-
+    target_fullPath = "/root/KL_Divergence/user_gauss_params/data/combine/"+username+"_comnbined.csv"
+    # perform_combine(transposed_inputs_dir,target_fullPath, username)
+    newline_combine(transposed_inputs_dir,target_fullPath, username)
     t1 = time.time()
     print(t1-t0)
     # i_and_freq(Dir)
