042ce1df1b621bd309a78626ab4962a6ea6aa5f1
Sheldonhh <xphuangsg@gmail.com>
2022-07-12 16:20:19 +0800

    update

--- user_gauss_params/py/A_raw_to_img_1.py
+++ user_gauss_params/py/A_raw_to_img_1.py
@@ -18,22 +18,21 @@ def main():
     #     "/root/KL_Divergence/user_gauss_params/data/user_4.csv", delimiter=",")
     # user_name = "user_4"
 
-    # user_1
+    # user_2
+    user_name = "user_2"
     numpy_array = np.loadtxt(
-        "/root/KL_Divergence/user_gauss_params/data/user_1.csv", delimiter=",")
-    user_name = "user_1"
+        "/root/KL_Divergence/user_gauss_params/data/"+user_name+".csv", delimiter=",")
 
-
-    true_arg_datapath = [
-        "/root/KL_Divergence/user_gauss_params/data/"]
-    raw_csv_argv = [true_arg_datapath[0]+user_name+".csv"]
-    raw_csv_path1 = raw_csv_argv[0]
-
+    true_datapath = "/root/KL_Divergence/user_gauss_params/data/"
 
     # raw to image
     for i in range(len(numpy_array)):
-        print(i)
-        raw_to_img([numpy_array[i]], user_name, str(i))
+        print()
+        if i != 0:
+            print(i)
+            raw_to_img([numpy_array[i]], true_datapath, user_name, str(i))
+        else: 
+            print(numpy_array[i])
     # img_to_fea(numpy_array, true_arg_datapath, )
     # img_to_fea_fast(numpy_array, true_arg_datapath, user_name, len(numpy_array))
 
@@ -42,29 +41,33 @@ def main():
     ########################################################################
 
 
-def raw_to_img(numpy_array, user_name, index_of_image):
+def raw_to_img(numpy_array, true_datapath, user_name, index_of_image):
     # print(raw_csv_argv)
     raw_csv_argv = ["/root/KL_Divergence/user_gauss_params/data/"+user_name+".csv"]
+    new_img_path = true_datapath+"image/"+user_name+"/"
+    if os.path.isdir(new_img_path) == False:
+        os.mkdir(new_img_path)
     raw_data_len = csv_to_jpeg.csv_to_jpeg(numpy_array, raw_csv_argv, index_of_image, user_name)
 
 
-def img_to_fea(numpy_array, true_argv, user_name):
-    new_fea_path = true_argv[0]+"features/"+user_name+"/"
-    if os.path.isdir(new_fea_path) == False:
-        os.mkdir(new_fea_path) # make dir for that user
-    nested_features = []
-    psedo_csv_path_img = ["/root/KL_Divergence/user_gauss_params/data/image/"+user_name+"/"+user_name+".csv"]
-    for i in range(len(numpy_array)):
-        if os.path.exists(new_fea_path+user_name+"_features_"+str(i)+".csv") == False:
-            single_file_feature = extract_features(psedo_csv_path_img,  str(i))
-            np.savetxt(new_fea_path+user_name+"_features_"+str(i)+".csv", single_file_feature)
-            nested_features.append(single_file_feature)
+
+# def img_to_fea(numpy_array, true_argv, user_name):
+#     new_fea_path = true_argv[0]+"features/"+user_name+"/"
+#     if os.path.isdir(new_fea_path) == False:
+#         os.mkdir(new_fea_path) # make dir for that user
+#     nested_features = []
+#     psedo_csv_path_img = ["/root/KL_Divergence/user_gauss_params/data/image/"+user_name+"/"+user_name+".csv"]
+#     for i in range(len(numpy_array)):
+#         if os.path.exists(new_fea_path+user_name+"_features_"+str(i)+".csv") == False:
+#             single_file_feature = extract_features(psedo_csv_path_img,  str(i))
+#             np.savetxt(new_fea_path+user_name+"_features_"+str(i)+".csv", single_file_feature)
+#             nested_features.append(single_file_feature)
             # print(i)
 
 
-def img_to_fea_fast(numpy_array, true_argv, user_name, lens):
-    true_datapath = "/root/KL_Divergence/user_gauss_params/data/"
-    extract_features_fast(true_datapath, user_name, lens)
+# def img_to_fea_fast(numpy_array, true_datapath, user_name, lens):
+#     true_datapath = "/root/KL_Divergence/user_gauss_params/data/"
+#     extract_features_fast(true_datapath, user_name, lens)
  
             # print(i)
 
--- user_gauss_params/py/B_img_and_fea_1.py
+++ user_gauss_params/py/B_img_and_fea_1.py
@@ -21,25 +21,24 @@ def main():
     # user_1
     numpy_array = np.loadtxt(
         "/root/KL_Divergence/user_gauss_params/data/user_1.csv", delimiter=",")
-    user_name = "user_1"
-
+    user_name = "user_2"
 
     true_arg_datapath = [
         "/root/KL_Divergence/user_gauss_params/data/"]
     raw_csv_argv = [true_arg_datapath[0]+user_name+".csv"]
     raw_csv_path1 = raw_csv_argv[0]
 
-
     # raw to image
     # for i in range(len(numpy_array)):
     #     raw_to_img([numpy_array[i]], user_name, str(i))
     # img_to_fea(numpy_array, true_arg_datapath, )
-    img_to_fea_fast(numpy_array, true_arg_datapath, user_name, len(numpy_array))
+    img_to_fea_fast(numpy_array, true_arg_datapath,
+                    user_name, len(numpy_array))
 
     t1 = time.time()
     print(t1-t0)
     ########################################################################
-    
+
     #  image to features
     # for i in range(len(numpy_array)):
     #     # numpy_array[i]
@@ -49,13 +48,10 @@ def main():
     #         np.savetxt(true_arg_datapath[0]+user_name+"_features_"+str(i)+".csv", single_file_feature)
     #         nested_features.append(single_file_feature)
     #         print(i)
-        # else:
-        #     break
-    
+    # else:
+    #     break
 
     # nested_features = []
-
-
 
     # true_arg_datapath = [
     #     "/root/KL_Divergence/user_gauss_params/data/"]
@@ -70,21 +66,24 @@ def main():
     #     print(col.tolist())
     #     # round_data_golang.round_data(raw_csv_argv,col.tolist())
     #     freq_to_gauss_golang.freq_to_gauss(raw_csv_argv, len(numpy_array), col, str(idx))
-        # break
-        
-    
+    # break
+
     # print(len(nested_features[0]))
 
     # pd.DataFrame(nested_features).T.to_csv(path+username+"_joints_"+".csv", header=False, index=False)
 
 # 74.2619972229003
+
+
 def raw_to_img(numpy_array, user_name, index_of_image):
     # print(raw_csv_argv)
-    raw_csv_argv = ["/root/KL_Divergence/user_gauss_params/data/"+user_name+".csv"]
-    raw_data_len = csv_to_jpeg.csv_to_jpeg(numpy_array, raw_csv_argv, index_of_image, user_name)
+    raw_csv_argv = [
+        "/root/KL_Divergence/user_gauss_params/data/"+user_name+".csv"]
+    raw_data_len = csv_to_jpeg.csv_to_jpeg(
+        numpy_array, raw_csv_argv, index_of_image, user_name)
     # return extract_features(raw_csv_argv, index_of_image)
     # print(single_file_feature)
-   
+
     # round_data_golang.round_data(raw_csv_argv)
     # countFreq_golang.count_freq(raw_csv_argv)
     # freq_to_gauss_golang.freq_to_gauss(raw_csv_argv, raw_data_len)
@@ -93,13 +92,15 @@ def raw_to_img(numpy_array, user_name, index_of_image):
 def img_to_fea(numpy_array, true_argv, user_name):
     new_fea_path = true_argv[0]+"features/"+user_name+"/"
     if os.path.isdir(new_fea_path) == False:
-        os.mkdir(new_fea_path) # make dir for that user
+        os.mkdir(new_fea_path)  # make dir for that user
     nested_features = []
-    psedo_csv_path_img = ["/root/KL_Divergence/user_gauss_params/data/image/"+user_name+"/"+user_name+".csv"]
+    psedo_csv_path_img = [
+        "/root/KL_Divergence/user_gauss_params/data/image/"+user_name+"/"+user_name+".csv"]
     for i in range(len(numpy_array)):
         if os.path.exists(new_fea_path+user_name+"_features_"+str(i)+".csv") == False:
             single_file_feature = extract_features(psedo_csv_path_img,  str(i))
-            np.savetxt(new_fea_path+user_name+"_features_"+str(i)+".csv", single_file_feature)
+            np.savetxt(new_fea_path+user_name+"_features_" +
+                       str(i)+".csv", single_file_feature)
             nested_features.append(single_file_feature)
             # print(i)
 
@@ -107,8 +108,9 @@ def img_to_fea(numpy_array, true_argv, user_name):
 def img_to_fea_fast(numpy_array, true_argv, user_name, lens):
     true_datapath = "/root/KL_Divergence/user_gauss_params/data/"
     extract_features_fast(true_datapath, user_name, lens)
- 
-            # print(i)
+
+    # print(i)
+
 
 if __name__ == "__main__":
     main()
